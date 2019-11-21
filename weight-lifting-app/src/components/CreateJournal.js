ffaf874ad3843ed9a2bc7cf1a6b4cbe9c5558a11
Kyle Leonard <kdleonard@icloud.com>
2019-11-20 20:00:15 -0600

    Working on the CreateJournal component
    installed a date-picker

--- weight-lifting-app/src/components/CreateJournal.js
+++ weight-lifting-app/src/components/CreateJournal.js
@@ -1,28 +1,37 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const CreateJournal = props => {
+  const [newJournal, setNewJournal] = useState({
+    date: "",
+    region: ""
+  });
 
-    const [newJournal, setNewJournal] = useState({
-        date: "",
-        region: ""
-    });
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
 
-    const handleSubmit= e =>{
-        e.preventDefault(); 
-    }
+  axios.post("restricted/journals/", props);
 
-    axios.post("restricted/journals/", props)
+  return (
+    <div className="field">
+      <div className="control">
+        <div className=""></div>
+      </div>
+    </div>
 
-    return (
-        <div class="control">
-  <div class="select">
-    <select>
-      <option>Select dropdown</option>
-      <option>With options</option>
-    </select>
-  </div>
-</div>
-    )
 
-}
+
+    <div className="field">
+      <div class="control">
+        <span class="select">
+          <select>
+            <option>Upper</option>
+            <option>Lower</option>
+            <option>Cardio/Core</option>
+          </select>
+        </span>
+      </div>
+    </div>
+  );
+};
